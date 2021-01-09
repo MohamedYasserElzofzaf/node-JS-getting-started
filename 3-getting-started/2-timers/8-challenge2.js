ccc4198504db55768c12d74a6cea46e704380e2d
Mohammed Yasser <mohammed.yasser.elzofzaf@gmail.com>
2021-01-09 17:55:00 +0200

    add the solution for challenge 2

--- 3-getting-started/2-timers/8-challenge2.js
+++ 3-getting-started/2-timers/8-challenge2.js
@@ -1,5 +1,10 @@
-// Print "Hello World"
-// Every second
-// And stop after 5 times
+let counter = 0;
+const timerId = setInterval(() => {
+    console.log("Hello World");
+    counter += 1;
 
-// After 5 times. Print "Done" and let Node exit.
+    if (counter === 5) {
+        console.log("Done");
+        clearInterval(intervalId);
+    }
+}, 1000);
